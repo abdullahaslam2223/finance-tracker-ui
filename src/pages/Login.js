46df57365a8c9eadc564cbdbd6d2cf411003666a
Abdullah Sheikh <96911855+abdullahaslam2223@users.noreply.github.com>
2023-08-18 16:34:30 +0500

    making login api request

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,41 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { setUserSession } from '../utils/common';
+import { API_BASE_URL } from '../config';
 
 function Login() {
     const navigate = useNavigate();
-    const username = useFormInput('');
+    const email = useFormInput('');
     const password = useFormInput('');
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const postData = {
+      email: email.value,
+      password: password.value
+    }
 
     const handleLogin = () => {
+      setError(null);
+      setLoading(true);
+      axios.post(API_BASE_URL + 'login', postData).then(response => {
+        const res = response.data;
+        setLoading(false);
+        setUserSession(res.data.token, res.data.user);
         navigate('/dashboard');
-      }
+      }).catch(error => {
+        setLoading(false);
+        // setError(error?.message || "Something went wrong. Please try again later.");
+        setError("Invalid Cradentials");
+      });
+    }
+
     return (
         <div>
           Login<br /><br />
           <div>
-            Username<br />
-            <input type="text" {...username} autoComplete="new-password" />
+            Email<br />
+            <input type="text" {...email} autoComplete="new-password" />
           </div>
           <div style={{ marginTop: 10 }}>
             Password<br />
