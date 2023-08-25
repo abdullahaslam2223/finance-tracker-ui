f4144fa5b3bb186a7c76354f9c0e097a10a80796
Abdullah Sheikh <96911855+abdullahaslam2223@users.noreply.github.com>
2023-08-26 00:26:59 +0500

    updating expense chart on realtime data

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,69 +1,73 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-
-
-const dailyExpenses = [
-    { date: '2023-08-01', amount: 100 },
-    { date: '2023-08-02', amount: 75 },
-    { date: '2023-08-03', amount: 1000 },
-    { date: '2023-08-04', amount: 500 },
-    { date: '2023-08-05', amount: 10 },
-    { date: '2023-08-08', amount: 20 },
-    { date: '2023-08-01', amount: 100 },
-    { date: '2023-08-02', amount: 75 },
-    { date: '2023-08-03', amount: 1000 },
-    { date: '2023-08-04', amount: 500 },
-    { date: '2023-08-05', amount: 10 },
-    { date: '2023-08-08', amount: 20 },
-    { date: '2023-08-01', amount: 100 },
-    { date: '2023-08-02', amount: 75 },
-    { date: '2023-08-03', amount: 1000 },
-    { date: '2023-08-04', amount: 500 },
-    { date: '2023-08-05', amount: 10 },
-    { date: '2023-08-08', amount: 20 },
-    { date: '2023-08-01', amount: 100 },
-    { date: '2023-08-02', amount: 75 },
-    { date: '2023-08-03', amount: 1000 },
-    { date: '2023-08-04', amount: 500 },
-    { date: '2023-08-05', amount: 10 },
-    { date: '2023-08-08', amount: 20 },
-    { date: '2023-08-01', amount: 100 },
-    { date: '2023-08-02', amount: 75 },
-    { date: '2023-08-03', amount: 1000 },
-    { date: '2023-08-04', amount: 500 }
-];
-
-const options = {
-    chart: {
-        type: 'column',
-    },
-    title: {
-        text: 'Daily Expenses',
-    },
-    xAxis: {
-        categories: dailyExpenses.map((expense) => expense.date),
-        title: {
-        text: 'Date',
-        },
-    },
-    yAxis: {
-        title: {
-        text: 'Amount',
-        },
-    },
-    series: [
-        {
-        name: 'Expense',
-        data: dailyExpenses.map((expense) => expense.amount),
-        },
-    ],
-};
+import { useState, useContext, useEffect } from 'react';
+import { API_BASE_URL } from '../utils/config';
+import axios from 'axios';
+import { getTokenHeader } from '../utils/common';
+import { AuthContext } from '../App';
+import Loader from './Loader';
 
 
 const ExpenseChart = () => {
+    const { token } = useContext(AuthContext);
+    const headers = getTokenHeader(token);
+    // console.log(headers);
+    const [dailyExpenses, setDailyExpenses] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    
+    const getChartTransactions = () => {
+        setLoading(true);
+        axios.get(API_BASE_URL + 'chart-transactions', { headers: headers }).then(response => {
+            const res = response.data;
+            setLoading(false);
+            setDailyExpenses(res.data);
+        }).catch(error => {
+            setLoading(false);
+            // Handle error
+        })
+    }
+
+    useEffect(() => {
+        getChartTransactions();
+    }, []);
+
+    const options = {
+        chart: {
+            type: 'column',
+        },
+        title: {
+            text: 'Daily Expenses',
+        },
+        xAxis: {
+            categories: dailyExpenses?.map((expense) => expense.date),
+            title: {
+            text: 'Date',
+            },
+        },
+        yAxis: {
+            title: {
+            text: 'Amount',
+            },
+        },
+        series: [
+            {
+            name: 'Expense',
+            data: dailyExpenses?.map((transaction) => transaction.expense),
+            },
+        ],
+    };
+
+    console.log(dailyExpenses);
+
     return (
       <div className='container shadow'>
-        <HighchartsReact highcharts={Highcharts} options={options} />
+        { loading ? (
+            <Loader />
+        ) : (
+                dailyExpenses && <HighchartsReact highcharts={Highcharts} options={options} />
+            )
+        }
       </div>
     );
 };
