4a8746445ae8c0207c257d9182a4f2319123add8
Abdullah Sheikh <96911855+abdullahaslam2223@users.noreply.github.com>
2023-08-26 01:32:04 +0500

    adding date filters

---
+++ src/components/DateFilter.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import '../styles/style.css';
+
+function DateFilter({ filters, handleStartDateChange, handleEndDateChange }) {
+
+  return (
+    <div className="date-filter-container">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="form-group">
+            <label htmlFor="startDate" className="date-filter-label">Start Date&nbsp;&nbsp;</label>
+            <DatePicker
+              id="startDate"
+              selected={filters.startDate}
+              onChange={handleStartDateChange}
+              dateFormat="yyyy-MM-dd"
+              className="form-control date-filter-input"
+              placeholderText="Select start date"
+            />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form-group">
+            <label htmlFor="endDate" className="date-filter-label">End Date&nbsp;</label>
+            <DatePicker
+              id="endDate"
+              selected={filters.endDate}
+              onChange={handleEndDateChange}
+              dateFormat="yyyy-MM-dd"
+              className="form-control date-filter-input"
+              placeholderText="Select end date"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DateFilter;
--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -6,31 +6,48 @@ import axios from 'axios';
 import { getTokenHeader } from '../utils/common';
 import { AuthContext } from '../App';
 import Loader from './Loader';
+import DateFilter from './DateFilter';
+import moment from 'moment';
 
 
 const ExpenseChart = () => {
     const { token } = useContext(AuthContext);
     const headers = getTokenHeader(token);
-    // console.log(headers);
     const [dailyExpenses, setDailyExpenses] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [filters, setFilters] = useState({
+        startDate: moment().startOf('month').toDate(),
+        endDate: moment().toDate()
+    });
 
+    const handleStartDateChange = (date) => {
+        setFilters({ ...filters, startDate: date });
+    };
+    
+    const handleEndDateChange = (date) => {
+        setFilters({ ...filters, endDate: date });
+    };
+
+    const formattedStartDate = moment(filters.startDate).format('YYYY-MM-DD');
+    const formattedEndDate = moment(filters.endDate).format('YYYY-MM-DD');
+    const queryParams = `start_date=${formattedStartDate}&end_date=${formattedEndDate}`;
+
+    const url = `${API_BASE_URL}chart-transactions?${queryParams}`;
     
     const getChartTransactions = () => {
         setLoading(true);
-        axios.get(API_BASE_URL + 'chart-transactions', { headers: headers }).then(response => {
+        axios.get(url, { headers }).then(response => {
             const res = response.data;
             setLoading(false);
             setDailyExpenses(res.data);
         }).catch(error => {
             setLoading(false);
-            // Handle error
         })
     }
 
     useEffect(() => {
         getChartTransactions();
-    }, []);
+    }, [filters]);
 
     const options = {
         chart: {
@@ -58,17 +75,21 @@ const ExpenseChart = () => {
         ],
     };
 
-    console.log(dailyExpenses);
-
     return (
-      <div className='container shadow'>
-        { loading ? (
-            <Loader />
-        ) : (
-                dailyExpenses && <HighchartsReact highcharts={Highcharts} options={options} />
-            )
-        }
-      </div>
+    <>
+        <div className='mb-5'>
+            <DateFilter filters={filters} handleStartDateChange={handleStartDateChange} handleEndDateChange={handleEndDateChange} />
+        </div>
+    
+        <div className='container shadow'>
+            { loading ? (
+                <Loader />
+            ) : (
+                    dailyExpenses && <HighchartsReact highcharts={Highcharts} options={options} />
+                )
+            }
+        </div>
+    </>
     );
 };
 
